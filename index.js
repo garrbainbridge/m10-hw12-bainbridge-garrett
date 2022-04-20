7fc2cdab1a49c4a7cc65447b42315c2ddd8fbf39
garrett bainbridge <gbainbridge@ufl.edu>
2022-04-20 15:25:03 -0400

    added js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // YOUR CODE HERE
+class Car {
+    constructor(make, model, year){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    honk() {
+        console.log('BEEP BEEP!')
+    }
+    performMaintenance() {
+        setTimeout(function() {
+            console.log('maintenance complete')
+            }, 3* 1000)
+    }
+};
+
+var mySweetRide = new Car ("Pontiac", "Fiero", 1988)
+mySweetRide.honk()
+mySweetRide.performMaintenance()
